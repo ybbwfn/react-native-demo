63c91d3eb55d483bf4b392e955a85b79f397784f
ybbwfn <ybbwfnta@126.com>
2019-12-11 17:15:23 +0800

    font字体图标

--- src/component/TabBar.js
+++ src/component/TabBar.js
@@ -1,12 +1,45 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View} from 'react-native';
 
-// import Icon from 'react-native-vector-icons/FontAwesome';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+// import Iconfont from 'react-native-vector-icons/Iconfont';
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 import Home from '../view/Home'
 import About from '../view/About'
+class IconWithBadge extends React.Component {
+  render() {
+    const { name, badgeCount, color, size } = this.props;
+    return (
+      <View style={{ width: 24, height: 24, margin: 5 }}>
+        <Ionicons name={name} size={size} color={color} />
+        {badgeCount > 0 && (
+          <View
+            style={{
+              // If you're using react-native < 0.57 overflow outside of parent
+              // will not work on Android, see https://git.io/fhLJ8
+              position: 'absolute',
+              right: -6,
+              top: -3,
+              backgroundColor: 'red',
+              borderRadius: 6,
+              width: 12,
+              height: 12,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Text style={{ color: 'white', fontSize: 10, fontWeight: 'bold' }}>
+              {badgeCount}
+            </Text>
+          </View>
+        )}
+      </View>
+    );
+  }
+}
+// 
 class HomeScreen extends React.Component {
   componentDidMount() {
 
@@ -20,7 +53,7 @@ class HomeScreen extends React.Component {
   }
 }
 
-class SettingsScreen extends React.Component {
+class HousekeepScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
@@ -29,36 +62,59 @@ class SettingsScreen extends React.Component {
     );
   }
 }
-
+class FindgsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <About />
+      </View>
+    );
+  }
+}
+class MyScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <About />
+      </View>
+    );
+  }
+}
+const HomeIconWithBadge = props => {
+  // You should pass down the badgeCount in some other ways like React Context API, Redux, MobX or event emitters.
+  return <IconWithBadge {...props} badgeCount={3} />;
+};
 const TabNavigator = createBottomTabNavigator(
   {
     Home: HomeScreen,
-    Settings: SettingsScreen,
+    Housekeep: HousekeepScreen,
+    Find: FindgsScreen,
+    My: MyScreen,
   },
-  // {
-  //   defaultNavigationOptions: ({ navigation }) => ({
-  //     tabBarIcon: ({ focused, horizontal, tintColor }) => {
-  //       const { routeName } = navigation.state;
-  //       let IconComponent = FontAwesome;
-  //       let iconName;
-  //       if (routeName === 'Home') {
-  //         iconName = `ios-information-circle${focused ? '' : '-outline'}`;
-  //         // Sometimes we want to add badges to some icons.
-  //         // You can check the implementation below.
-  //         IconComponent = HomeIconWithBadge;
-  //       } else if (routeName === 'Settings') {
-  //         iconName = `ios-options`;
-  //       }
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let IconComponent = Ionicons;
+        let iconName;
+        if (routeName === 'Home') {
+          iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+          // Sometimes we want to add badges to some icons.
+          // You can check the implementation below.
+          IconComponent = HomeIconWithBadge;
+        } else if (routeName === 'Housekeep') {
+          iconName = `ios-options`;
+        }
 
-  //       // You can return any component that you like here!
-  //       return <IconComponent name={iconName} size={25} color={tintColor} />;
-  //     },
-  //   }),
-  //   tabBarOptions: {
-  //     activeTintColor: 'tomato',
-  //     inactiveTintColor: 'gray',
-  //   },
-  // }
+        // You can return any component that you like here!
+        return <IconComponent name={iconName} size={25} color={tintColor} />;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: 'tomato',
+      inactiveTintColor: 'gray',
+    },
+  }
 );
 
 export default createAppContainer(TabNavigator);
